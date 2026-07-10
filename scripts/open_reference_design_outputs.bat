058287b301fc997604c660383f54f158f1a78a8a
Hani <haniyeh.moslemi2019@gmail.com>
2026-07-10 18:47:02 +0330

    Remove obsolete reference design shortcut

--- scripts/open_reference_design_outputs.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0\.."
-start "" outputs\figures\reference_mission_map.png
-start "" outputs\figures\reference_hopper_energy_coverage.png
-start "" outputs\figures\reference_force_trajectory_3d.png
-start "" outputs\figures\reference_parameter_traceability.png
-start "" outputs\animations\reference_mission_animation_contact_sheet.png
-endlocal
