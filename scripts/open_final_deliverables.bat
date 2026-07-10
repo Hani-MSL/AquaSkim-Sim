37a71bb42008fe7470d9df34277a2f17b65cd12d
Hani <haniyeh.moslemi2019@gmail.com>
2026-07-10 18:45:23 +0330

    Remove obsolete output shortcut

--- scripts/open_final_deliverables.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal EnableExtensions
-cd /d "%~dp0\.."
-echo [BLOCKED] Final delivery disabled pending Release Gate.
-echo [BLOCKED] Do not open or distribute Word/ZIP deliverables at this stage.
-endlocal & exit /b 2
